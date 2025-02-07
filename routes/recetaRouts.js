79f3e0d07a9e2d04c921829977be16b1cce2feef
Marco Sanchez <marcosanchez590@gmail.com>
2025-02-06 21:02:23 -0600

    Actualizacion de rutas, Falta corregir algunas

--- routes/recetaRouts.js
+++ routes/recetaRouts.js
@@ -3,7 +3,9 @@ const recipeController = require('../controllers/recetaController');
 const router = express.Router();
 
 router.get('/', recipeController.getReceta);
-router.post('/', recipeController.createReceta);
+router.get("/:id", recipeController.getRecetaID);
+router.post('/createReceta', recipeController.createReceta);
+router.put("/:id", recipeController.updateReceta);
 router.delete('/:id', recipeController.deleteReceta);
 
 module.exports = router;
